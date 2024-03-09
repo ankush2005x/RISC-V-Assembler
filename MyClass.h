ba7be25d7eb4ff033c57caa0b6c290310b8e900a
satvik6391765316 <127044449+satvik6391765316@users.noreply.github.com>
2024-03-09 15:57:24 +0530

    Revert "Delete MyClass.h"
    This reverts commit b47c72bb790478b47358375f8063279ee6a22552.

---
+++ MyClass.h
@@ -0,0 +1,24 @@
+// MyClass.h
+#ifndef MYCLASS_H
+#define MYCLASS_H
+typedef long long ll;
+#include <vector>
+#include <string>
+using namespace std;
+
+class myclass
+{
+    // private:
+    //     vector<vector<string>> input;
+    //     ll store;
+
+    // public:
+    //     myclass(vector<vector<std::string>>, ll store);
+    //     void DataSegment();
+    //     void D_WORD(vector<string>, ll);
+
+public:
+    myclass(std::string);
+};
+
+#endif
