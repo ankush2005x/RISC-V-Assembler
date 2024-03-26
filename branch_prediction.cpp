40867500d033fc7fc876968ba1588d79b84021ef
satvik6391765316 <127044449+satvik6391765316@users.noreply.github.com>
2024-03-26 17:15:01 +0530

    made a few changes in branch prediction code

--- branch_prediction.cpp
+++ branch_prediction.cpp
@@ -3,35 +3,29 @@ using namespace std;
 
 #include "branch_prediction.h"
 
-branch_prediction::branch_prediction(string path)
+branch_prediction::branch_prediction(vector<vector<string>> code)
 {
-    fileInput.open(path);
     last_pc = 0;
     offset = 0;
     last_instr = "";
     total_predicted = 0;
     correct_predicted = 0;
-    if (!fileInput.is_open())
+    for (auto val : code)
     {
-        error = 1;
-        return;
-    }
-    while (getline(fileInput, line))
-    {
-        branch_taken(line);
+        branch_taken(val);
     }
     if (total_predicted != 0)
         cout << "accuracy when the branch is always taken : " << ((correct_predicted * 1.0) / total_predicted) * 100 << "%" << endl;
     else
         cout << "No branches are present in the code" << endl;
 }
-void branch_prediction::branch_taken(string line)
+void branch_prediction::branch_taken(vector<string> line)
 {
-    string this_instr = line.substr(34, 3);
+    string this_instr = line[4];
     if (last_instr == "beq" || last_instr == "bne" || last_instr == "blt" || last_instr == "bge")
     {
         total_predicted++;
-        int this_pc = hexatodec(line.substr(12, 8));
+        int this_pc = hexatodec(line[2]);
         if (last_pc + offset == this_pc)
         {
             correct_predicted++;
@@ -39,8 +33,8 @@ void branch_prediction::branch_taken(string line)
     }
     if (this_instr == "beq" || this_instr == "bne" || this_instr == "blt" || this_instr == "bge")
     {
-        int offset = stoi(line.substr(55));
-        last_pc = hexatodec(line.substr(12, 8));
+        int offset = stoi(line[7]);
+        last_pc = hexatodec(line[2]);
     }
     last_instr = this_instr;
 }
@@ -66,8 +60,4 @@ int branch_prediction::hexatodec(string hexVal)
         }
     }
     return dec_val;
-}
-int main()
-{
-    branch_prediction obj("assembly2.s");
 }
