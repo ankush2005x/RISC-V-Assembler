f274257049f516e3934c4857a030d0e2d9528fa1
Ashish-Gupta-2003 <143898106+Ashish-Gupta-2003@users.noreply.github.com>
2024-03-27 01:00:51 +0530

    Update build.cpp
    Some bugs got fixed

--- build.cpp
+++ build.cpp
@@ -2,30 +2,60 @@
 using namespace std;
 #include "parse.h"
 #include "converter.h"
+#include "branch_prediction.h"
 
 ofstream fout;
 
 int main()
 {
     // fileInput.open for reading the input file.
-    fout.open("output.mc");
-    string path = "assembly.s";
+    cout << "Press 1 for Assembly to Machine Code Convertion. \nPress 2 for Branch Table Prediction Analysis.\n";
+    int mode;
+    cin >> mode;
 
-    fout << "Data Segment Memory: \n";
-    parser tokens(path);
-    if (tokens.error == 1)
+    fout.open("output.mc");
+    string path;
+    if (mode == 1)
     {
-        fout << tokens.raiseError << "\n";
+        path = "assembly.s";
+    }
+    else if (mode == 2)
+    {
+        path = "trace.s";
+    }
+    else
+    {
+        cout << "Wrong Option Selected. Code Exited.\n";
         return 1;
     }
+    parser tokens(path, mode);
+    
+    if (mode == 1)
+    {
+        fout << "Data Segment Memory: \n";
+        
+        if (tokens.error == 1)
+        {
+            fout << tokens.raiseError << "\n";
+            return 1;
+        }
+        fout << "\n\nAssembly Instructions:\n";
+        tokens.print(mode);
 
-    fout << "\n\nAssembly Instructions:\n";
-    tokens.print();
+        fout << "\n\nMachine Code:\n";
+        converter obj(tokens.code);
+        obj.assemblytomachine();
+    }
+    else
+    {
+        vector<vector<string>> branchData;
+        for (auto items : tokens.code)
+        {
+            branchData.push_back(items.first);
+        }
+        branch_prediction obj(branchData);
+    }
 
-    fout << "\n\nMachine Code:\n";
-
-    converter obj(tokens.code);
-    obj.assemblytomachine();
     fout.close();
     return 0;
 }
